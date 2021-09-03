c2e59eca2558d2225e862d12e9f7c5a5b0cfd951
Ahmet Demir <demir45@yahoo.com>
2021-09-03 12:24:51 +0300

    version_02

--- src/test/java/junit5Tutorials/J06PerformanceTest.java
+++ src/test/java/junit5Tutorials/J06PerformanceTest.java
@@ -12,5 +12,8 @@ public class J06PerformanceTest {
     void performance(){
         assertTimeout(Duration.ofSeconds(1), () -> IntStream.rangeClosed(0, 100000).
                 forEach(System.out::print));
+
+        //        assertTimeout(Duration.ofMillis(10), () -> DoubleStream.iterate(1, t->t+1).
+        //        limit(1000000).sum());
     }
 }
